10bc2eb804f03855a055153b848c5bedca6685e2
AnujKourav <anujkourav63@gmail.com>
2025-05-14 00:00:16 +0530

    Adding JS

---
+++ JS/Lecture54/script.js
@@ -0,0 +1,13 @@
+alert("Hello")
+
+var a = prompt("Enter a number")
+
+console.log("your number "+ a);
+
+var isTrue = confirm("Are you sure to leave ")
+if(isTrue){
+    console.log("leaving")
+}
+else{
+    console.log("no")
+}
